af65df5eb2d081d7865223ba085ba9622f254e2f
zyzy <tayzhengyu@gmail.com>
2025-08-09 04:24:19 +0800

    test

--- yellowstone-grpc-geyser/src/plugin.rs
+++ yellowstone-grpc-geyser/src/plugin.rs
@@ -206,6 +206,11 @@ impl GeyserPlugin for Plugin {
                     unreachable!("ReplicaAccountInfoVersions::V0_0_1 is not supported")
                 }
                 ReplicaTransactionInfoVersions::V0_0_2(info) => info,
+                ReplicaTransactionInfoVersions::V0_0_3(_info) => {
+                    return Err(GeyserPluginError::Custom(
+                        "ReplicaTransactionInfoVersions::V0_0_3 is not supported".into(),
+                    ));
+                }
             };
 
             let message = Message::Transaction(MessageTransaction::from_geyser(transaction, slot));
